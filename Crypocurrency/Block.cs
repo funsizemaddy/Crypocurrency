68438b68fecbc81ea0842b75fadbe56e7622a03c
funsizemaddy <90282709+funsizemaddy@users.noreply.github.com>
2022-02-03 12:03:47 -0700

    Add project files.

---
+++ Crypocurrency/Block.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using EllipticCurve;
+
+namespace Crypocurrency
+{
+    class Block
+    {
+        public int Index { get; set; }
+        public string Prevhash { get; set; }
+        public string TimeStamp { get; set; }
+     
+        public string Hash { get; set; }
+
+        public int Nonce { get; set; }
+        public List<Transaction> Transactions { get; set; }
+
+        //Contructor
+        public Block(int index, string timestamp, List<Transaction> transactions, string previousHash = "")
+        {
+            this.Index = index;
+            this.TimeStamp = timestamp;
+            this.Transactions = transactions;
+            this.Prevhash = previousHash;
+            this.Hash = CalculateHash();
+            this.Nonce = 0;
+
+
+        }
+        public string CalculateHash()
+        {
+            string blockData = this.Index + this.Prevhash + this.TimeStamp + this.Transactions.ToString() + this.Nonce;
+            byte[] blockBytes = Encoding.ASCII.GetBytes(blockData);
+            byte[] hashBytes = SHA256.Create().ComputeHash(blockBytes);
+            return BitConverter.ToString(hashBytes).Replace("-", "");
+        }
+
+        public void Mine(int difficulty)
+        {
+            while (this.Hash.Substring(0,difficulty) != new string ('0', difficulty))
+            {
+                this.Nonce++;
+                this.Hash = this.CalculateHash();
+                Console.WriteLine("Mining " + this.Hash);
+            }
+            Console.WriteLine("Block has been mined: " + this.Hash);
+        }
+
+    }
+}
---
+++ Crypocurrency/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+using EllipticCurve;
+
+
+namespace Crypocurrency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrivateKey key1 = new PrivateKey();
+            PublicKey wallet1 = key1.publicKey();
+
+            PrivateKey key2 =  new PrivateKey();
+            PublicKey wallet2 = key2.publicKey();
+
+            BlockChain doublecoin = new BlockChain(2,100);
+
+            Console.WriteLine("Start the miner.");
+            doublecoin.MinePendingTransactions(wallet1);
+            Console.WriteLine(
+                "\nBalance of wallet1 is $" + 
+                doublecoin.GetBalanceofWallet(wallet1).ToString());
+
+            Transaction tx1 = new Transaction(wallet1, wallet2, 10);
+            tx1.SignTransaction(key1);
+            doublecoin.addPendingTransaction(tx1);
+
+            Console.WriteLine("Start the miner.");
+            doublecoin.MinePendingTransactions(wallet2);
+            Console.WriteLine(
+                "\nBalance of wallet1 is $" +
+                doublecoin.GetBalanceofWallet(wallet1).ToString());
+            Console.WriteLine(
+                "\nBalance of wallet2 is $" +
+                doublecoin.GetBalanceofWallet(wallet2).ToString());
+
+
+            string blockJSON = JsonConvert.SerializeObject(doublecoin, Formatting.Indented);
+            Console.WriteLine(blockJSON);
+
+            if (doublecoin.IsChainValid())
+            {
+                Console.WriteLine("BlockChain is valid");
+            }
+            else
+            {
+                Console.WriteLine("BlockChian is NOT valid");
+            }
+        }
+    }
+    
+    
+}
